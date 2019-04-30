7265c9c03792ff7062683665ab190ccd0985fb7c
YuriiKovalenko <wrestlerok99@gmail.com>
2019-04-30 21:11:12 +0300

    added ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { MainComponent } from './pages/main/main.component';
 import { MainModule } from './pages/main/main.module';
 import { CommonModule } from './common/common.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgtUniversalModule } from '@ng-toolkit/universal';
 
 @NgModule({
   declarations: [AppComponent, MainComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    NgtUniversalModule,
+    BrowserAnimationsModule,
     MainModule,
     HttpClientModule,
     CommonModule,
